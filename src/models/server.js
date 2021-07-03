64b73d2e3009b5f2acbee69e97e75589dd3b0e29
joseluisseg05 <joseluistimy@gmail.com>
2021-07-03 17:19:59 -0500

    Home

--- src/models/server.js
+++ src/models/server.js
@@ -44,7 +44,7 @@ class Server {
         this.app.use( express.json());
 
         //directorio publico
-        this.app.use(express.static('public'));
+        this.app.use(express.static('src/public'));
 
     }
 
